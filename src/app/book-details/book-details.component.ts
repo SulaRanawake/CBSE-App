20dc64bb8a7f7764aba4a58e34cc291e211d1501
Sulakshana Ranawake <sula.ranawake@gmail.com>
2023-05-08 00:01:13 +0530

    Component 3 - book details component

---
+++ src/app/book-details/book-details.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Book } from '../book.model';
+
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.css']
+})
+export class BookDetailsComponent {
+  @Input() selectedBook: Book | null = null;
+}
